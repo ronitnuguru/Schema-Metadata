519195dc5d9b10579e2f08145ff33b58266bafba
Ronit Nuguru <ronit.nuguru@gmail.com>
2020-03-19 16:55:30 -0700

    schema metadata

---
+++ force-app/main/default/lwc/chooseObject/chooseObject.js
@@ -0,0 +1,26 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ChooseObject extends LightningElement {
+
+    @track searchTerm;
+    @track objectSchemaSuccess = false;
+    @track objectApiName;
+
+    
+    handleObjectSearchTermChange(event){
+        this.searchTerm = event.target.value;
+    }
+
+    handleObjectSubmit(){
+        console.log(this.searchTerm);
+        this.objectSchemaSuccess = true;
+        this.objectApiName = this.searchTerm;
+    }
+
+    handleCancel(event) {
+        event.preventDefault();
+        this.objectSchemaSuccess = false;
+        this.searchTerm = '';
+    }
+    
+}
---
+++ force-app/main/default/lwc/objectSchema/objectSchema.js
@@ -0,0 +1,154 @@
+import { LightningElement, api, wire, track } from "lwc";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
+
+export default class ObjectSchema extends LightningElement {
+  @api objectApiName;
+  @track objectMetaData;
+  @track columns;
+  @track data = [];
+  @track likeState = false;
+  @track generateQueryState = false;
+  @track queryGenerateState = false;
+
+  @track childRelationships = [];
+  @track dependentFields = [];
+  @track fields;
+  @track cardTitle;
+  @track completeArray;
+  @track filteredArray;
+  @track generatedQuery;
+
+  @track filteredArray;
+  @track completeArray;
+
+  
+
+  @wire(getObjectInfo, { objectApiName: "$objectApiName" })
+  objectRecordData({ error, data }) {
+    if (error) {
+      console.log(error);
+    } else if (data) {
+      this.cardTitle = `Schema for  ${this.objectApiName}`;
+      this.objectMetaData = data;
+      //console.log(data);
+      this.dataTableSetup(this.objectMetaData);
+    }
+  }
+
+  
+
+  dataTableSetup(tableData) {
+
+    
+    this.columns = [
+      { label: "Field", fieldName: "apiName" },
+      { label: "Type", fieldName: "dataType" },
+      { label: "Parent", fieldName: "parent" }
+    ];
+    this.childRelationships = tableData.childRelationships;
+    this.dependentFields = tableData.dependentFields;
+    this.fields = tableData.fields;
+    let filteredArray = [];
+    let completeArray = [];
+    const dataEntries = Object.entries(this.fields).filter(field => {
+      const filteredFields = field[1];
+
+      if (
+        filteredFields.createable &&
+        filteredFields.updateable &&
+        filteredFields.apiName !== "OwnerId"
+      ) {
+        filteredArray = [
+          {
+            apiName: filteredFields.apiName,
+            dataType: filteredFields.dataType,
+            parent: filteredFields.relationshipName,
+            referenceToInfos: [filteredFields.referenceToInfos]
+          },
+          ...filteredArray
+        ];
+      }
+
+      completeArray = [
+        {
+          apiName: filteredFields.apiName,
+          dataType: filteredFields.dataType,
+          parent: filteredFields.relationshipName,
+          referenceToInfos: [filteredFields.referenceToInfos]
+        },
+        ...completeArray
+      ];
+      return filteredFields;
+    });
+    this.data = completeArray;
+
+    this.completeArray = completeArray;
+    this.filteredArray = filteredArray;
+
+    console.log(filteredArray);
+    console.log(dataEntries);
+
+    /*
+    console.log("Data Entries");
+    
+    console.log(this.childRelationships);
+    console.log(this.dependentFields);
+    console.log(this.fields);
+
+    const objectKeys = Object.keys(this.fields);
+    console.log(objectKeys);
+    */
+  }
+
+  handleFilter() {
+    if (this.likeState === true) {
+      this.data = this.completeArray;
+    } else {
+      this.data = this.filteredArray;
+    }
+    this.likeState = !this.likeState;
+  }
+
+  handleCancel() {
+    this.dispatchEvent(new CustomEvent("cancel"));
+  }
+
+  handleGenerateQuery() {
+    this.generateQueryState = true;
+    this.queryCreation();
+  }
+
+  queryCreation(){
+    let selectClause = `SELECT `;
+    // eslint-disable-next-line no-unused-vars
+    let fieldReference = ``;
+    this.filteredArray.forEach(field => {
+      if(!field.parent){
+        selectClause = `${selectClause} ${field.apiName}, `;
+      }
+      else {
+        const { nameFields } = field.referenceToInfos[0][0];
+        if(nameFields){
+          if(nameFields.length === 1) {
+            fieldReference = nameFields[0];
+          }
+          else {
+            if(nameFields.includes("Name")){
+              fieldReference = "Name"
+            }
+            else {
+              fieldReference = nameFields[0];
+            }
+          }
+          selectClause = `${selectClause} ${field.parent}.${fieldReference},`
+        }
+      } 
+    });
+    
+    selectClause = selectClause.substring(0, selectClause.length-1);
+    console.log(selectClause);
+    const fromClause = ` FROM ${this.objectApiName}`;
+    this.generatedQuery = `${selectClause} ${fromClause}`
+  }
+  
+}
